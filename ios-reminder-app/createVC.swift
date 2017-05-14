2c40df83b7a258a63c749adda301a133189f8f60
William Guo <williamguo5@gmail.com>
2017-05-15 02:41:33 +1000

    Changed coreData objects to store a primary key. Selecting a reminder from shows its details in the create view controller.

--- ios-reminder-app/createVC.swift
+++ ios-reminder-app/createVC.swift
@@ -16,28 +16,57 @@ class createVC: UIViewController {
     @IBOutlet weak var reminderText: UITextField!
     @IBOutlet weak var addNotesText: UITextView!
     
-    var chosenReminderURI = URL(string: "")
+    var chosenReminderID:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if (chosenReminderURI != URL(string: "")) {
-            print(chosenReminderURI)
+        if (chosenReminderID != 0) {
+            print(chosenReminderID)
+            // predicate
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Reminder")
+            fetchRequest.predicate = NSPredicate(format: "id = %d", chosenReminderID)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            do {
+                let results = try context.fetch(fetchRequest)
+                if results.count > 0 {
+                    print("----- successfully retrieved specific object from coredata")
+                    for result in results as! [NSManagedObject] {
+                        if let name = result.value(forKey: "name") as? String {
+                            reminderText.text = name
+                        }
+                        if let notes = result.value(forKey: "notes") as? String {
+                            addNotesText.text = notes
+                        }
+                    }
+                }
+            } catch {
+                print("Error fetching specified object from coredata")
+            }
         }
 
         // Do any additional setup after loading the view.
     }
 
     @IBAction func saveButtonClicked(_ sender: Any) {
+        let pid = UserDefaults.standard.object(forKey: "reminderPID") as? Int
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
         
         let newReminder = NSEntityDescription.insertNewObject(forEntityName: "Reminder", into: context)
         newReminder.setValue(reminderText.text, forKey: "name")
         newReminder.setValue(addNotesText.text, forKey: "notes")
-        
+        newReminder.setValue(pid, forKey: "id")
+
+        UserDefaults.standard.set((pid! + 1), forKey: "reminderPID")
+        UserDefaults.standard.synchronize()
         do {
             try context.save()
+            
             print("Save Successful")
         } catch {
             print("Error: could not save")
--- ios-reminder-app/tableVC.swift
+++ ios-reminder-app/tableVC.swift
@@ -14,19 +14,25 @@ class tableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var tableView: UITableView!
     
     struct reminder {
-        var objectURI : URL
+        var id : Int
         var name : String
     }
     
     var reminderArray = [reminder]()
     
-    var selectedReminderURI = URL(string: "")
+    var selectedReminderID = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.dataSource = self
         tableView.delegate = self
+        
+        // set primary key if not exists
+        if (UserDefaults.standard.object(forKey: "reminderPID") == nil) {
+            UserDefaults.standard.set(0, forKey: "reminderPID")
+            UserDefaults.standard.synchronize()
+        }
         
         retrieveInfo()
     }
@@ -45,19 +51,19 @@ class tableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
             
             if (results.count > 0) {
                 for result in results as! [NSManagedObject] {
-                    if let name = result.value(forKey: "name") as? String {
-                        let object = reminder(objectURI: result.objectID.uriRepresentation(), name: name)
+                    if let id = result.value(forKey: "id") as? Int {
+                        let name = result.value(forKey: "name") as? String
+                        let object = reminder(id: id, name: name!)
                         self.reminderArray.append(object)
-                        
-                        print (name)
-                        print (result.objectID.uriRepresentation())
+                        print(id)
+                        print(name!)
                     }
                     
                     self.tableView.reloadData()
                 }
             }
         } catch {
-            print("Error fetching from coreDate")
+            print("Error fetching from coreData")
         }
     }
     
@@ -72,14 +78,14 @@ class tableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        self.selectedReminderURI = reminderArray[indexPath.row].objectURI
+        self.selectedReminderID = reminderArray[indexPath.row].id
         performSegue(withIdentifier: "toCreateVC", sender: nil)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if (segue.identifier == "toCreateVC") {
             let destinationVC = segue.destination as! createVC
-            destinationVC.chosenReminderURI = selectedReminderURI
+            destinationVC.chosenReminderID = selectedReminderID
         }
     }
 
@@ -87,5 +93,39 @@ class tableVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         performSegue(withIdentifier: "toCreateVC", sender: nil)
     }
 
+    @IBAction func editButtonClicked(_ sender: Any) {
+//        if (self.tableView.isEditing){
+//            self.tableView.isEditing = false
+////            self.editButtonItem.title = "Edit"
+//        } else {
+//            self.tableView.isEditing = true
+////            self.editButtonItem.title = "Done"
+//        }
+        print ("clear button clicked")
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Reminder")
+        fetchRequest.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            
+            if (results.count > 0) {
+                for result in results as! [NSManagedObject] {
+                    context.delete(result)
+                }
+            }
+        } catch {
+            print("Error fetching from coreData")
+        }
+        do {
+            try context.save()
+            self.tableView.reloadData()
+        } catch {
+            print ("Error saving delete operation")
+        }
+
+    }
 }
 
